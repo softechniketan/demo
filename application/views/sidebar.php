f8ec26add9c85fef80f00b35f84efe005c3ac307
softechniketan <phawadeprashant@gmail.com>
2023-05-18 12:01:01 +0530

    first commit

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -174,7 +174,7 @@
 
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
+    <!-- Brand Logo -->  
     <a href="index3.html" class="brand-link">
       <img src="<?php echo base_url('AdminLTE');?>/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
